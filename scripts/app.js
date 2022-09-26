34b7ab3e2bf17e10c6bc342c811cee83e52a54b3
luu-jennifer <jenniferluu87@gmail.com>
2022-09-25 19:45:39 -0700

    added prompt for email in scripts

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+//slide out menu
 const menuButton = document.getElementById("menuButton");
 const slideOutNav = document.getElementById("slideOutNav");
 const menuClose = document.getElementById("menuClose");
@@ -8,3 +9,7 @@ const hideSlideOutNav = () => slideOutNav.style.left = "-506px";
 
 menuButton.addEventListener("click", showSlideOutNav);
 menuClose.addEventListener("click", hideSlideOutNav);
+
+// prompt email on page load
+
+const email = prompt("Subscribe Now and Receive 10% OFF");
